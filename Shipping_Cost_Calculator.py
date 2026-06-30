655c47b75006709982977e0828e5a3c9fe4b1e63
saj-312 <s.abolhassani@hotmail.com>
2026-06-30 21:54:10 +1000

    Revert "Added Another 3rd new Line to Py file"
    This reverts commit e8f71488b5983c1882801a1e1f4131d574663199.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -11,7 +11,4 @@ shipping_cost = weight * rate
 print(f"Shipping Cost: {shipping_cost} USD")
 
  # Here is a new update by <saj-312>
- # Here is another update by <saj-312>
-  # Here is another update by <saj-312>
-
 
